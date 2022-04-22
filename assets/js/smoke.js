d8fbdb0c03626016927cad8fef5448a8c6c93ead
Rafael Santos <RafaelChefe@users.noreply.github.com>
2022-04-22 23:47:08 +1000

    Updating smoke on the water 2.0

--- assets/js/smoke.js
+++ assets/js/smoke.js
@@ -3,4 +3,7 @@ function printSmokeOnTheWater() {
   document.write("On the Lake Geneva shoreline <br>");
   document.write("To make records with a mobile <br>");
   document.write("We didn't have much time <br>");
+
+  document.write("Frank Zappa and the mothers");
+  document.write("Were at the best place around");
 }
