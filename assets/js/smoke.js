a39fdeb0935088dfea21e95338284a375d502c20
Rafael Santos <RafaelChefe@users.noreply.github.com>
2022-04-23 00:01:02 +1000

    New updates for this library

--- assets/js/smoke.js
+++ assets/js/smoke.js
@@ -2,8 +2,8 @@ function printSmokeOnTheWater() {
   document.write("We all came out to MONTREAUX <br>");
   document.write("On the Lake Geneva shoreline <br>");
   document.write("To make records with a mobile <br>");
-  document.write("We didn't have much time <br>");
+  document.write("We didn't have much time <br><br>");
 
-  document.write("Frank Zappa and the mothers");
-  document.write("Were at the best place around");
+  document.write("Frank Zappa and the mothers <br>");
+  document.write("Were at the best place around <br>");
 }
